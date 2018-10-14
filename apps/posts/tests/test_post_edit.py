1f73e676832cfb834b22bbba33b90ae4968a084e
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-10-13 22:40:28 -0300

    fix TestPostUpdate

--- apps/posts/tests/test_post_edit.py
+++ apps/posts/tests/test_post_edit.py
@@ -1,14 +1,22 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from .. import models
+
 
 class TestPostUpdate(TestCase):
 
     def setUp(self):
-        self.response = self.client.get(reverse('posts:edit'))
+        self.post = models.Post.objects.create(
+            title='title',
+            content='content'
+        )
+        self.response = self.client.get(
+            reverse('posts:edit', kwargs={'pk': self.post.pk})
+        )
 
     def test_get(self):
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        self.assertTemplateUsed(self.response, 'posts/edit.html')
+        self.assertTemplateUsed(self.response, 'posts/form.html')
