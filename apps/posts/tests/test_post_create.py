d4301420909edc24f3c79d42e0188ed41d591e79
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-10-13 21:56:58 -0300

    Add TestPostCreateInvalid

--- apps/posts/tests/test_post_create.py
+++ apps/posts/tests/test_post_create.py
@@ -48,3 +48,25 @@ class TestPostCreateNew(TestCase):
 
     def test_save_subscription(self):
         self.assertTrue(models.Post.objects.exists())
+
+
+class TestPostCreateInvalid(TestCase):
+
+    def setUp(self):
+        self.response = self.client.post(reverse('posts:create'), {})
+        self.form = self.response.context['form']
+
+    def test_post(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'posts/create.html')
+
+    def test_has_form(self):
+        self.assertIsInstance(self.form, forms.PostForm)
+
+    def test_form_has_errors(self):
+        self.assertTrue(self.form.errors)
+
+    def test_dont_save_subscription(self):
+        self.assertFalse(models.Post.objects.exists())
