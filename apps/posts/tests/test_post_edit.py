b66cb03664fc648d8dbb66caac26298a57a32360
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-10-13 22:50:50 -0300

    Add TestPostEditPost

--- apps/posts/tests/test_post_edit.py
+++ apps/posts/tests/test_post_edit.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .. import models
+from .. import forms, models
 
 
-class TestPostUpdate(TestCase):
+class TestPostEdit(TestCase):
 
     def setUp(self):
         self.post = models.Post.objects.create(
@@ -14,9 +14,47 @@ class TestPostUpdate(TestCase):
         self.response = self.client.get(
             reverse('posts:edit', kwargs={'pk': self.post.pk})
         )
+        self.form = self.response.context['form']
 
     def test_get(self):
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
         self.assertTemplateUsed(self.response, 'posts/form.html')
+
+    def test_has_form(self):
+        self.assertIsInstance(self.form, forms.PostForm)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_html(self):
+        tags = (
+            ('<form', 1),
+            ('<input', 3),
+            ('type="text"', 1),
+            ('type="submit"', 1),
+        )
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.response, text, count)
+
+
+class TestPostEditPost(TestCase):
+
+    def setUp(self):
+        data = {'title': 'title', 'content': 'content'}
+        self.client.post(reverse('posts:create'), data)
+        self.response = self.client.post(
+            reverse('posts:edit', kwargs={'pk': 1}),
+            data
+        )
+
+    def test_post(self):
+        self.assertRedirects(
+            self.response,
+            reverse('posts:detail', kwargs={'pk': 1})
+        )
+
+    def test_save_subscription(self):
+        self.assertTrue(models.Post.objects.exists())
