88eb7e97569b6712d29386d51f50a7bc623e146d
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-10-15 22:11:47 -0300

    add slug field

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -6,6 +6,7 @@ from contrib import utils
 class Post(models.Model):
     title = models.CharField('title', max_length=120)
     content = models.TextField('content')
+    slug = models.SlugField('slug', unique=True)
     image = models.ImageField(
         'image',
         upload_to=utils.upload_file_path,
@@ -28,6 +29,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = utils.get_unique_slug(self, 'title', 'slug')
+        super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse("posts:detail", kwargs={"pk": self.pk})
--- contrib/utils.py
+++ contrib/utils.py
@@ -2,6 +2,8 @@ import hashlib
 import os
 from datetime import datetime
 
+from django.utils.text import slugify
+
 
 def upload_file_path(instance, filename):
     basename = os.path.basename(filename)
@@ -9,3 +11,18 @@ def upload_file_path(instance, filename):
     name = hashlib.md5(name.encode()).hexdigest()
     date = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     return f'{instance.id}/{date}-{name}{ext}'
+
+
+def get_unique_slug(instance, field_name, slug_field_name):
+    slug = slugify(getattr(instance, field_name))
+    unique_slug = slug
+    extension = 1
+    ModelClass = instance.__class__
+
+    while ModelClass._default_manager.filter(
+        **{slug_field_name: unique_slug}
+    ).exists():
+        unique_slug = f'{slug}-{extension}'
+        extension += 1
+
+    return unique_slug
